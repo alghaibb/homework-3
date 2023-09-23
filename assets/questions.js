6d272825f105ad5250c5b7e3a0bba3161636a25d
Moudi <mjaderi97@gmail.com>
2023-09-23 16:00:30 +1000

    Added a questions.js for the quiz box

---
+++ assets/questions.js
@@ -0,0 +1,59 @@
+// ARRAY FOR PASSING NUMBERS, QUESTIONS, OPTIONS & ANSWERS
+let questions = [
+    {
+      numb: 1,
+      question: "What does HTML stand for?",
+      answer: "Hyper Text Markup Language",
+      options: [
+        "Hyper Transfer Markup Language",
+        "Hyper Text Markup Language",
+        "High-Level Text Markup Language",
+        "Hyperlink and Text Markup Language",
+      ]
+    },
+    {
+      numb: 2,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheets",
+      options: [
+        "Cascading Style Sheets",
+        "Computer Style Sheets",
+        "Creative Style Sheets",
+        "Colorful Style Sheets",
+      ]
+    },
+    {
+      numb: 3,
+      question: "Which programming language is commonly used for building the backend of web applications?",
+      answer: "Ruby",
+      options: [
+        "Java",
+        "JavaScript",
+        "Python",
+        "Ruby",
+      ]
+    },
+    {
+      numb: 4,
+      question: "What is the purpose of the HTML <a> tag?",
+      answer: "Creating hyperlinks to other web pages",
+      options: [
+        "Adding images to a webpage",
+        "Creating hyperlinks to other web pages",
+        "Formatting text as bold",
+        "Embedding videos",
+      ]
+    },
+    {
+      numb: 5,
+      question: "Which term refers to the process of adapting a website's design to fit various screen sizes and devices?",
+      answer: "Responsiveness",
+      options: [
+        "Responsiveness",
+        "Redirection",
+        "Rendering",
+        "Refactoring",
+      ]
+    }
+  ];
+  
